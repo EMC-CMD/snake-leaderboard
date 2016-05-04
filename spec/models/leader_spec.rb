febca133515fcab5c1b188765834f33486a6a653
Joshua Aresty <joshua.aresty@emc.com>
2016-05-04 11:36:54 -0400

    Modify test for leaders_by_specific_time method
    Signed-off-by: Thinh Nguyen <thinh.nguyen@emc.com>

--- spec/models/leader_spec.rb
+++ spec/models/leader_spec.rb
@@ -9,10 +9,12 @@ RSpec.describe Leader, type: :model do
 
   let(:leaderboard_validator) { double }
   context 'get leader by specific time' do
-    Timecop.travel(Time.zone.local(2016, 5, 3, 17, 0, 0)) do
-      l1 = Leader.put(twitter_handle: 'l1', score: 23, validated: true)
-      expect(Leader.leaders_by_specific_time(2016, 5, 3, 17, 0, 0).to_a).to eql([l1])
-      expect(Leader.leaders_by_specific_time(2016, 5, 3, 16, 59, 59).to_a).to eql([])
+    it 'returns top 10 leaders of that specific time' do
+      Timecop.travel(Time.zone.local(2016, 5, 3, 17, 0, 0)) do
+        l1 = Leader.put(twitter_handle: 'l1', score: 23, validated: true)
+        expect(Leader.leaders_by_specific_time(2016, 5, 3, 17, 0, 0).to_a).to eql([l1])
+        expect(Leader.leaders_by_specific_time(2016, 5, 3, 16, 59, 59).to_a).to eql([])
+      end
     end
   end
 
